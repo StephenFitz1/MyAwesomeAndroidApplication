314c881be4457fe972dc2a3d3c30ff1a0c66ae18
StephenFitz1 <93268638+StephenFitz1@users.noreply.github.com>
2024-02-08 13:53:32 +0000

    Initial commit

---
+++ app/src/main/java/com/example/myawesomeandroidapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myawesomeandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myawesomeandroidapplication.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+    //Make Button that sends a message
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
